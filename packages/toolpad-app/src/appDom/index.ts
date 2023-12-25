850ba25bbc22b53506564aecc0124b2eef86a97e
wenhao.liu <wenhao.liu@elestyle.jp>
2023-12-25 16:37:15 +0900

    更新appDom typescript提示

--- packages/toolpad-app/src/appDom/index.ts
+++ packages/toolpad-app/src/appDom/index.ts
@@ -503,7 +503,7 @@ export function validateNodeName(name: string, disallowedNames: Set<string>, kin
 }
 
 export function createNode<T extends AppDomNodeType>(
-  dom: AppDom,
+  _: AppDom,
   type: T,
   init: AppDomNodeInitOfType<T>,
 ): AppDomNodeOfType<T> {
@@ -570,7 +570,7 @@ export function createElement<P>(
 export function getDescendants(dom: AppDom, node: AppDomNode): readonly AppDomNode[] {
   const children: readonly AppDomNode[] = Object.values(getChildNodes(dom, node))
     .flat()
-    .filter(Boolean);
+    .filter(Boolean) as any;
   return [...children, ...children.flatMap((child) => getDescendants(dom, child))];
 }
 
